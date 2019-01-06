5fb495c97182afdc5ef6dea95294ce44c8212f42
min_2271 <min_2271@naver.com>
2019-01-06 23:06:52 +0900

    Vanila_Js 복습

---
+++ Vanila_JS/19_01_06_실습/index.js
@@ -0,0 +1,13 @@
+alert("작동이 잘되고 있습니다~");
+console.log("작동이 잘되고 있습니다~");
+
+
+const monday = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
+console.log(dayOfWeek);
