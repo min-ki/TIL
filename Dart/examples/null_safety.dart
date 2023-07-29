eeafb3d9ec8b4027aa5ac9832c3ca42e8c013a8e
min-ki <secreata77@gmail.com>
2023-07-29 23:58:45 +0900

    [Dart] null safety 예제 코드 작성

---
+++ Dart/examples/null_safety.dart
@@ -0,0 +1,29 @@
+// Null Safety
+
+void nullableType() {
+    int? count; // non-nullable 초기값은 null
+    print(count); // null
+
+    count = 10;
+    print(count); // 10
+}
+
+void nonNullableType() {
+    int count; // non-nullable 초기값은 null
+    count = 10;
+    print(count); // 10
+}
+
+void nonNullableTypeError() {
+    int count; // non-nullable 초기값은 null
+    
+    // Dart/examples/variables.dart:19:11: Error: Non-nullable variable 'count' must be assigned before it can be used.
+    print(count); // 초기화전에 사용하면 에러발생
+}
+
+
+void main() {
+    nullSafety();
+    nonNullableType();
+    // nonNullableTypeError();
+}
